7ae25f7885bcb4ce5ab127cb775740600798008a
Bucadebepo <77507575+Trismania@users.noreply.github.com>
2024-09-24 17:29:50 +0200

    DONE

--- scripts/healthbar.gd
+++ scripts/healthbar.gd
@@ -8,9 +8,12 @@ func get_hurt():
 	if health == 4:
 		health -= 1
 		animated_sprite_2d.play("three")
+		print("I now have 3 hearts")
 	if health == 3:
 		health -= 1
 		animated_sprite_2d.play("two")
+		print("I now have 2 hearts")
 	if health == 2:
 		health -= 1
 		animated_sprite_2d.play("one")
+		print("I now have 1 hearts")
